e1196b91339d10a1c4ff7159ebd6ab2c039e7bc1
Vikram Chowdary <vpairmi@deloitte.com>
2018-11-22 14:21:41 +0530

    first commit

---
+++ ResearchKitPOC/ResearchKitPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResearchKitPOC
+//
+//  Created by Pairmi, Vikram (US - Bengaluru) on 11/22/18.
+//  Copyright © 2018 vikram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
